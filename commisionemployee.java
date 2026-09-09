6593a6203f0c9924d476a5fd31949e99f2d9addc
Daniel Dinh <danieldinh@cpp.edu>
2026-09-09 15:26:24 -0700

    Wrote the commisionemployee subclass adding in the relevant attributes for this specific subclass

---
+++ commisionemployee.java
@@ -0,0 +1,26 @@
+public class commisionemployee extends employee{
+    private double rate;
+    private double gross_sales;
+
+    public commisionemployee(String firstname, String lastname, String ssn, double rate, double gross_sales){
+        super(firstname, lastname, ssn);
+        this.rate = rate;
+        this.gross_sales = gross_sales;
+    }
+
+    public double get_rate(){
+        return rate;
+    }
+
+    public double get_gross_sales(){
+        return gross_sales;
+    }
+
+    public void set_rate(double new_rate){
+        this.rate = new_rate;
+    }
+
+    public void set_gross_sales(double new_gross){
+        this.gross_sales = new_gross;
+    }
+}
