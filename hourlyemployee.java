7c78709b9a529f696b6f072c286f25db922217e9
Daniel Dinh <danieldinh@cpp.edu>
2026-09-09 15:18:53 -0700

    Have copied over the salaried-employees class and have converted all of the relevant points for hourly employee, specifically adding in the wages and hours_worked instead of salary)

---
+++ hourlyemployee.java
@@ -0,0 +1,26 @@
+public class hourlyemployee extends employee{
+    private double wage;
+    private int hours_worked;
+
+    public hourlyemployee(String firstname, String lastname, String ssn, double wage, int hours_worked){
+        super(firstname, lastname, ssn);
+        this.wage = wage;
+        this.hours_worked = hours_worked;
+    }
+
+    public double get_wage(){
+        return wage;
+    }
+
+    public int get_hours(){
+        return hours_worked;
+    }
+
+    public void set_wage(double wage){
+        this.wage = wage;
+    }
+
+    public void set_hours(int new_hours_worked){
+        this.hours_worked = new_hours_worked;
+    }
+}
