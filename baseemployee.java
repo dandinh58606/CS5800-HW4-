e10dd712efa6980f2736b54de58cf4b79d31517a
Daniel Dinh <danieldinh@cpp.edu>
2026-09-09 16:18:25 -0700

    Added in the final toString method to make it easier to print out the relevant info in the driver file

--- baseemployee.java
+++ baseemployee.java
@@ -13,4 +13,10 @@ public class baseemployee extends employee{
     public void set_base_sal(double new_base_sal){
         this.base_salary = new_base_sal;
     }
+
+    public String toString() {
+    return get_firstname() + " " + get_lastname() + 
+        " | SSN: " + get_ssn() + 
+        " | Base Salary: $" + String.format("%.2f", base_salary);
+}
 }
