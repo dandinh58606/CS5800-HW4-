7bf3e247b9a5cea2cd525ae58d5424418638a76c
Daniel Dinh <danieldinh@cpp.edu>
2026-09-09 15:10:56 -0700

    had to rename the file to match the class name

--- employee.java
+++ employee.java
@@ -1,9 +1,9 @@
-public class Employee {
+public class employee{
     private String first_name;
     private String last_name;
     private String social_security_num;
 
-    public Employee(String first_name, String last_name, String ssn){
+    public employee(String first_name, String last_name, String ssn){
         this.first_name = first_name;
         this.last_name = last_name;
         this.social_security_num = ssn;
