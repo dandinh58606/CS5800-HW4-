309a403d0632364ef8af0b8ba10d51ee1078aae9
Daniel Dinh <danieldinh@cpp.edu>
2026-09-09 15:14:40 -0700

    Removed the unneeded import statement as currently all files are in the same directory and have also changed the weekly_salary attribute to a double to better represent money versus a string

--- salariedemployee.java
+++ salariedemployee.java
@@ -1,18 +1,16 @@
-import employee;
-
 public class salariedemployee extends employee{
-    private String weekly_salary;
+    private double weekly_salary;
 
-    public salariedemployee(String firstname, String lastname, String ssn, String weekly_salary){
+    public salariedemployee(String firstname, String lastname, String ssn, double weekly_salary){
         super(firstname, lastname, ssn);
         this.weekly_salary = weekly_salary;
     }
 
-    public String get_weekly(){
+    public double get_weekly(){
         return weekly_salary;
     }
 
-    public void set_weekly(String weekly_salary){
+    public void set_weekly(double weekly_salary){
         this.weekly_salary = weekly_salary;
     }
 }
