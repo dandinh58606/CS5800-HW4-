6786b1f666fd7d12a595b3ebfe38cf7fc650d841
Daniel Dinh <danieldinh@cpp.edu>
2026-09-10 16:52:36 -0700

    Initial writing of the attributes and constructor of the Course class

--- aggregation/Course.java
+++ aggregation/Course.java
@@ -1,5 +1,13 @@
 package aggregation;
 
 public class Course {
-    
+    private String course_name;
+    private Instructor[] instructors;
+    private Textbook[] textbooks;
+
+    public Course(String class_name, Instructor[] instructor_list, Textbook[] book_list){
+        this.course_name = class_name;
+        this.instructors = instructor_list;
+        this.textbooks = book_list;
+    }
 }
