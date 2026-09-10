d9412c66dd9ebee6879a8498017d2885b464ae3b
Daniel Dinh <danieldinh@cpp.edu>
2026-09-10 13:02:59 -0700

    Finished working on the constructor, mutators, and accessors for the Freelancer class

--- interfaces/Freelancer.java
+++ interfaces/Freelancer.java
@@ -1,5 +1,61 @@
 package interfaces;
 
 public class Freelancer implements Payable{
-    
+    private String firstname;
+    private String lastname;
+    private double hourly_rate;
+    private double hours_worked;
+
+    public Freelancer(String first, String last, double hour_rate, double working_hours){
+        this.firstname = first;
+        this.lastname = last;
+        set_hourly_rate(hour_rate);
+        set_working_hours(working_hours);
+    }
+
+    public String get_first_name(){
+        return firstname;
+    }
+
+    public void set_first_name(String new_first_name){
+        this.firstname = new_first_name;
+    }
+
+    public String get_last_name(){
+        return lastname;
+    }
+
+    public void set_last_name(String new_last_name){
+        this.lastname = new_last_name;
+    }
+
+    public double get_hourly_rate(){
+        return hourly_rate;
+    }
+
+    public void set_hourly_rate(double new_hourly_rate){
+        if(new_hourly_rate < 0){
+            throw new IllegalArgumentException("Hourly Rate cannot be negative: " + new_hourly_rate);
+        }
+        this.hourly_rate = new_hourly_rate;
+    }
+
+    public double get_working_hours(){
+        return hours_worked;
+    }
+
+    public void set_working_hours(double new_working_hours){
+        if(new_working_hours < 0){
+            throw new IllegalArgumentException("Amount of hours worked cannot be negative: " + new_working_hours);
+        }
+        this.hours_worked = new_working_hours;
+    }
+
+    public double calculatePayment(){
+        double wage;
+        if(this.hours_worked <= 40){
+            wage = this.hours_worked * this.hourly_rate;
+        }
+        return wage;
+    }
 }
