c0c43afdbe1ff172c85f1c619ac0a09e90b2621e
Daniel Dinh <danieldinh@cpp.edu>
2026-09-10 16:02:38 -0700

    Wrote out the functionality of creating a list of ships and then printing them out in a loop

--- polymorphism/driver.java
+++ polymorphism/driver.java
@@ -1,5 +1,15 @@
 package polymorphism;
 
 public class driver {
-    
+    public static void main(String[] args) {
+        Ship[] shiplist = new Ship[3];
+
+        shiplist[0] = new Ship("Queen Mary", "1920");
+        shiplist[1] = new CruiseShip("Quantum of the Seas", "2020", 100000);
+        shiplist[2] = new CargoShip("The Royal Scurvy", "1531", 25);
+
+        for(Ship ship : shiplist){
+            ship.print();
+        }
+    }
 }
