abe4be640e04a122f56b46ac1bbca5412520285f
Daniel Dinh <danieldinh@cpp.edu>
2026-09-10 16:27:33 -0700

    Created the constructor for Textbook object

--- aggregation/Textbook.java
+++ aggregation/Textbook.java
@@ -4,4 +4,10 @@ public class Textbook {
     private String title;
     private String author;
     private String publisher;
+
+    public Textbook(String book_title, String book_author, String book_publisher){
+        this.title = book_title;
+        this.author = book_author;
+        this.publisher = book_publisher;
+    }
 }
