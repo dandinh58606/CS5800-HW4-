2efb5df3200214533917511764a16e1da0830f48
Daniel Dinh <danieldinh@cpp.edu>
2026-09-10 16:36:50 -0700

    Created the mutators and accessors for all attributes of Textbook

--- aggregation/Textbook.java
+++ aggregation/Textbook.java
@@ -10,4 +10,28 @@ public class Textbook {
         this.author = book_author;
         this.publisher = book_publisher;
     }
+
+    public String get_title(){
+        return title;
+    }
+
+    public void set_title(String new_title){
+        this.title = new_title;
+    }
+
+    public String get_author(){
+        return author;
+    }
+
+    public void set_author(String new_author){
+        this.author = new_author;
+    }
+
+    public String get_publisher(){
+        return publisher;
+    }
+
+    public void set_publisher(String new_publisher){
+        this.publisher = new_publisher;
+    }
 }
