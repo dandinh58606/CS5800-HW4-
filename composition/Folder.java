1f648be61bd9502db41c8bb287ada169b063bc06
Daniel Dinh <danieldinh@cpp.edu>
2026-09-10 20:03:51 -0700

    Updated the print to differentiate between folders '*' and files '-' in the print statement

--- composition/Folder.java
+++ composition/Folder.java
@@ -38,9 +38,9 @@ public class Folder {
     }
 
     public void print(String spacing){
-        System.out.println(spacing + "📁 " + folder_name);
+        System.out.println(spacing + "* " + folder_name);
         for(File file: files){
-            System.out.print(spacing + "  📄 ");
+            System.out.print(spacing + "  - ");
             file.print();
         }
         for(Folder folder : subfolders){
