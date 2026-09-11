fcfbe5b3576d3502f5427954ebc96c72bc87dfe5
Daniel Dinh <danieldinh@cpp.edu>
2026-09-10 18:26:57 -0700

    Added in the needed mutators and accessors for the file class

--- composition/File.java
+++ composition/File.java
@@ -8,4 +8,20 @@ public class File {
         this.file_name = f_name;
         this.file_type = f_type;
     }
+
+    public String get_file_name(){
+        return file_name;
+    }
+
+    public void set_file_name(String new_name){
+        this.file_name = new_name;
+    }
+
+    public String get_file_type(){
+        return file_type;
+    }
+
+    public void set_file_type(String new_type){
+        this.file_type = new_type;
+    }
 }
