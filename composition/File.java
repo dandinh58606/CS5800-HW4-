cb9cab7e854c3c9536b24dffaed7380e9a92db9c
Daniel Dinh <danieldinh@cpp.edu>
2026-09-10 18:22:18 -0700

    Initial creation and commit of the File.java to be used to created the file class. Also wrote the attributes and constructor for the file object

---
+++ composition/File.java
@@ -0,0 +1,11 @@
+package composition;
+
+public class File {
+    private String file_name;
+    private String file_type;
+
+    public File(String f_name, String f_type){
+        this.file_name = f_name;
+        this.file_type = f_type;
+    }
+}
